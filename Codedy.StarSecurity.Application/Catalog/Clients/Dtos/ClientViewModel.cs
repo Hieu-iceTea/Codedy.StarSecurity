0d8a35c53753872b32a23259d258012c13f43307
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2021-03-13 09:21:20 +0700

    NEW: Create functions to get data

---
+++ Codedy.StarSecurity.Application/Catalog/Clients/Dtos/ClientViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codedy.StarSecurity.Application.Catalog.Clients.Dtos
+{
+    public class ClientViewModel
+    {
+    }
+}
---
+++ Codedy.StarSecurity.Application/Catalog/Users/IManageUserService.cs
@@ -0,0 +1,20 @@
+﻿using Codedy.StarSecurity.Application.Catalog.Users.Dtos;
+using Codedy.StarSecurity.Application.Dtos;
+using Codedy.StarSecurity.Data.Entiies;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codedy.StarSecurity.Application.Catalog.Users
+{
+    public interface IManageUserService
+    {
+        Task<int> Create(UserCreateRequest request);
+        Task<int> Update(UserEditRequest request);
+        Task<int> Delete(int Id);
+        Task<User> Detail(string ID);
+        Task<List<UserViewModel>> GetAll();
+        Task<PagedViewModel<UserViewModel>> GetAllPaging(string KeySearch, int pageIndex, int pageSize);    
+    }
+}
