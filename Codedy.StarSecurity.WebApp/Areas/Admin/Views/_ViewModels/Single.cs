c8b9648fc8b7ff0290349932e616cf561ef3698f
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-03-15 04:08:26 +0700

    Create: Folder
    - _ViewModels
    - Components

---
+++ Codedy.StarSecurity.WebApp/Areas/Admin/Views/_ViewModels/Single.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codedy.StarSecurity.WebApp.Areas.Admin.Views._ViewModels
+{
+    public class Single
+    {
+    }
+}
