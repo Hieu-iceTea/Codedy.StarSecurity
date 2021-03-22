be424242654f95f946eab070829abe88237fff78
Tuan Pham <60849085+tuanpth1909@users.noreply.github.com>
2021-03-22 22:32:33 +0700

    new: validate career

--- Codedy.StarSecurity.WebApp/Models/Database/Entities/Career.cs
+++ Codedy.StarSecurity.WebApp/Models/Database/Entities/Career.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,17 @@ namespace Codedy.StarSecurity.WebApp.Models.Database.Entities
     public class Career
     {
         public Guid Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Image { get; set; }
+        [Required]
         public string Salary { get; set; }
+        [Required]
         public string WorkAddress { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public DateTime ExpirationDate { get; set; }
-
         public DateTime CreatedAt { get; set; }
         public string CreatedBy { get; set; }
         public DateTime UpdatedAt { get; set; }
