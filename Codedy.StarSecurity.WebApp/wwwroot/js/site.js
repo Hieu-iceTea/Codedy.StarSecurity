8318919dc8425b8598492bf579b6b6b23bb5342e
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2021-04-02 14:16:27 +0700

    UPDATE: tách css, js ra file riêng

--- Codedy.StarSecurity.WebApp/wwwroot/js/site.js
+++ Codedy.StarSecurity.WebApp/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+// = = = = = = = = = = = = = = = = Phần này xử lý ảnh thumbnail cho <input type=image> = = = = = = = = = = = = = = = =
+function changeImg(input) {
+    //Nếu như tồn thuộc tính file, đồng nghĩa người dùng đã chọn file mới
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        //Sự kiện file đã được load vào website
+        reader.onload = function (e) {
+            //Thay đổi đường dẫn ảnh
+            $('#thumbnail').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+//Khi click #thumbnail thì cũng gọi sự kiện click #image
+$(document).ready(function () {
+    $('#thumbnail').click(function () {
+        $('#image').click();
+    });
+});
 
-// Write your JavaScript code.
