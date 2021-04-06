6471c79b400a8a3f5b72c87777853746a2c4eb16
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2021-04-06 10:14:07 +0700

    UPDATE: edit detail career in clinet

---
+++ Codedy.StarSecurity.WebApp/Views/_ViewModels/CarrerModel.cs
@@ -0,0 +1,14 @@
+﻿using Codedy.StarSecurity.WebApp.Models.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codedy.StarSecurity.WebApp.Views._ViewModels
+{
+    public class CarrerModel
+    {
+        public List<Career> Careers { get; set; }
+        public Career Career { get; set; }
+    }
+}
